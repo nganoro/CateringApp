3df986207fa391c34df0bca1321ef64ed6a4ccad
nganoro <50967358+nganoro@users.noreply.github.com>
2020-06-23 15:13:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cateringapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cateringapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher_photo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+}
